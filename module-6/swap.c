29e69f5413e6b51deaebe8b733d5102c77b96f86
Jared Kahn <jared.kahn@salesforce.com>
2026-08-30 22:38:16 -0400

    broken_swap and output pdfs and pngs

--- module-6/swap.c
+++ module-6/swap.c
@@ -1,20 +1,28 @@
- #include <stdio.h>
+#include <stdio.h>
 
 void swap(int *a, int *b) {
-  printf("Before swap: x = %d, y = %d\n", *a, *b);
-
   int temp = *b;
   *b = *a;
   *a = temp;
+}
 
-  printf("After swap: x = %d, y = %d\n", *a, *b);
+void broken_swap(int a, int b) {
+  int temp = b;
+  b = a;
+  a = temp;
 }
 
 int main() {
   int a = 10;
   int b = 20;
 
+  printf("Before swap: x = %d, y = %d\n", a, b);
   swap(&a, &b);
+  printf("After swap: x = %d, y = %d\n", a, b);
 
+  printf("\n---- broken swap ----\n");
+  printf("Before swap: x = %d, y = %d\n", a, b);
+  broken_swap(a, b);
+  printf("After swap: x = %d, y = %d\n", a, b);
   return 0;
 }
